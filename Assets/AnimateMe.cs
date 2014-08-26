f922e3d16f6f98025345c9900781697061cb8220
Graham Reeves <graham@grahamreeves.com>
2014-08-26 22:30:30 +0100

    Initial demo project with 2 cameras & gyro scope

---
+++ Assets/AnimateMe.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateMe : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Vector3 NewRot = transform.localEulerAngles;
+		NewRot.x += 1.0f;
+		NewRot.y += 5.0f;
+		//NewRot.z += 1.0f;
+		transform.localEulerAngles = NewRot;
+	}
+}
---
+++ Assets/StereoCamera.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class StereoCamera : MonoBehaviour {
+
+	public float		EyeSpacing = 0.4f;
+	private GameObject	camParent;	//	runtime parent for parent transform
+
+	void Start () {
+		//	dont let screen go to sleep
+		Screen.sleepTimeout = 0;
+
+		Input.gyro.enabled = true;
+
+		Transform originalParent = transform.parent; // check if this transform has a parent
+		camParent = new GameObject ("camParent"); // make a new parent
+		camParent.transform.position = transform.position; // move the new parent to this transform position
+		transform.parent = camParent.transform; // make this transform a child of the new parent
+		camParent.transform.parent = originalParent; // make the new parent a child of the original parent
+		camParent.transform.eulerAngles = new Vector3(90,90,0);
+	}
+
+
+	void Update () {
+
+		//	gr: move to reflection!
+		Transform Left = transform.FindChild ("Left Camera");
+		Transform Right = transform.FindChild ("Right Camera");
+		if (Left != null) {
+			Left.localPosition = new Vector3( -EyeSpacing, 0, 0 );
+		}
+		if (Right != null) {
+			Right.localPosition = new Vector3( EyeSpacing, 0, 0 );
+		}
+
+		//	rotate camera to match gyro
+		Quaternion camRot = Input.gyro.attitude * new Quaternion(0,0,1,0);
+		transform.localRotation = camRot;
+	}
+
+}
